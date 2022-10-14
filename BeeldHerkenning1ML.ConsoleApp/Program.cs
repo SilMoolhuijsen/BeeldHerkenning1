4b3b0ff546797e2b994276f2b391f6e5ca9db9f2
silmo <silmo@DESKTOP-KOKIHFO>
2022-10-14 22:17:08 +0200

    Add project files.

---
+++ BeeldHerkenning1ML.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using BeeldHerkenning1ML.Model;
+using static BeeldHerkenning1ML_ConsoleApp.MLModel1;
+
+namespace Beeldherkenning1
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Debug.WriteLine(" Object is not valid for this category.");
+
+            // Add input data
+
+            var input = new ModelInput();
+
+            input.ImageSource = "C:\\Users\\silmo\\OneDrive\\Desktop\\School\\Informatica\\AI Image Recognition\\Image Datasets for Training\\wolves\\_106348479_mediaitem106348478.jpg";
+
+            // Load  model and predict output of sample data
+
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Debug.WriteLine(result.Prediction);
+
+            Console.WriteLine(result.Prediction);
+
+        }
+    }
+}
